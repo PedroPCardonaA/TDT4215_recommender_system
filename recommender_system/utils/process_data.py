5fc55db77a96e7827cc1387d996bbaabd3d1ce9d
Pedro Cardona <pedropca@stud.ntnu.no>
2025-01-25 17:40:31 +0100

    Refactor process_item_data function: optimize topic encoding and remove redundant ranking logic

--- recommender_system/utils/process_data.py
+++ recommender_system/utils/process_data.py
@@ -20,10 +20,6 @@ def process_item_data(item_data: pl.DataFrame) -> pl.DataFrame:
     ]
     item_data = item_data.with_columns(subcategory_columns).drop("subcategory")
 
-    item_data = item_data.with_columns(
-        (pl.col("article_type").rank("dense") - 1).alias("article_type_ranked")
-    )
-
     vector_length = len(item_data[0, "document_vector"]) 
     vector_columns = [
         pl.col("document_vector").list.get(i).alias(f"vector_{i+1}")
@@ -35,21 +31,36 @@ def process_item_data(item_data: pl.DataFrame) -> pl.DataFrame:
         pl.col("article_type").rank("dense")-1
     )
 
-    max_topics = item_data["topics"].map_elements(lambda s: len(s),return_dtype=int).max()
 
-    unique_topics = set(topic for row in item_data["topics"] for topic in row)
+
+    # Calculate the maximum number of topics per row
+    max_topics = item_data["topics"].map_elements(lambda s: len(s), return_dtype=int).max()
+    print(f"max topics: {max_topics}")
+
+    # Create a mapping of unique topics to integer labels
+    unique_topics = set(topic for row in item_data["topics"].to_list() for topic in row)
     topic_to_label = {topic: i for i, topic in enumerate(unique_topics)}
+    print(f"unique topics: {unique_topics}")
 
+    # Encode topics into integer labels
     item_data = item_data.with_columns(
-        pl.col("topics").map_elements(lambda topics: [topic_to_label[topic] for topic in topics], return_dtype=list).alias("encoded_topics")
+        pl.col("topics").map_elements(lambda s: [topic_to_label[topic] for topic in s]).alias("encoded_topics")
     )
 
+    # Add individual topic columns
     for i in range(max_topics):
         item_data = item_data.with_columns(
-            pl.col("encoded_topics").map_elements(lambda x: x[i] if i < len(x) else None, return_dtype=int).alias(f"topic_{i+1}")
+            pl.col("encoded_topics").map_elements(lambda s: s[i] if i < len(s) else None, return_dtype=int).alias(f"topic_{i+1}")
         )
 
+    item_data = item_data.drop(["topics", "encoded_topics", "topic_11"])
 
-    item_data = item_data.drop(["topics", "encoded_topics"])
+
+
+
+    for i in range(1, len(item_data.columns)):
+        item_data = item_data.with_columns(
+            (pl.col(item_data.columns[i]) - item_data[item_data.columns[i]].mean()) / item_data[item_data.columns[i]].std()
+        )
 
     return item_data
