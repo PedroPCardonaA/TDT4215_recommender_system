758ad2da98476a5f6c36847eda3c6882aa608da7
flippi321 <90700334+flippi321@users.noreply.github.com>
2025-01-28 12:43:55 +0100

    Added a basic fit for testing purposes

--- recommender_system/models/user_based.py
+++ recommender_system/models/user_based.py
@@ -21,10 +21,9 @@ class CollaborativeReccomender:
         self.read_time_weight = read_time_weight
 
 
-    def calculate_impression_scores(self) -> pl.DataFrame:
+    def add_impression_scores(self) -> pl.DataFrame:
         '''
-        Creates a score based on the user's behavior (scroll percentage and read time).
-        The formula is:
+        Creates a score based on the user's behavior (scroll percentage and read time). Using the formula:
         impression_score = (scroll_percentage * scroll_percentage_weight) + (read_time * read_time_weight)
 
         Returns
@@ -32,11 +31,27 @@ class CollaborativeReccomender:
         pl.DataFrame
             A DataFrame with an additional column `impression_score`.
         '''
-        # Calculate the impression score using the given weights
-        updated_impressions = self.impressions.with_columns(
+        self.impressions = self.impressions.with_columns(
             (
                 pl.col("scroll_percentage") * self.scroll_percentage_weight +
                 pl.col("read_time") * self.read_time_weight
             ).alias("impression_score")
         )
-        return updated_impressions
+        return self.impressions
+
+    def fit(self):
+        '''
+        Creates a user-user cosine similarity matrix and uses this to 
+
+        Prepares user and item profiles by computing average user feature vectors
+        (user_profiles) and unique item feature vectors (item_profiles).
+        Also calculates norms for each user and item vector to speed up
+        cosine similarity.
+        '''
+        self.add_impression_scores()
+
+        return self.impressions
+
+    def predict(self, user_id: int, article_id: int) -> float:
+        
+        return 0.0
